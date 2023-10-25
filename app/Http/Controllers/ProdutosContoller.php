82217c2bfd40625755ed7e6d5f3738e5076e3c97
renato_peres300@yahoo.com.br <RenatoPeres100>
2023-10-25 15:21:57 -0300

    puxando do bd

--- app/Http/Controllers/ProdutosContoller.php
+++ app/Http/Controllers/ProdutosContoller.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Produto;
 
 class ProdutosContoller extends Controller
 {
     public function index() {
-        return 'produtos';
+        $findProduto = Produto::all();
+        return view('pages.produtos.paginacao', compact('findProduto'));
     }
 }
